527bfa98f2deb52699c7a816f26803a954a7122f
Raulino Neto <raulino.neto@endurance.com>
2020-02-12 21:25:56 -0300

    Make a basic documentation

--- docker/start.server.sh
+++ docker/start.server.sh
@@ -11,5 +11,4 @@ fi
 
 gomon --exec "make build" &
 make build
-mkdir -p .data/dynamodb/ && touch .data/dynamodb/shared-local-instance.db && chmod 0777 -R .data
 lambda-local start --host $HOST s--port $PORT --volume $VOLUME_APP --network partner-location-api_partners-network --env .local.env
